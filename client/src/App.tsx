9922ca435dfda5a9e784699a3bf904c353af5fab
cannaAriel <cannaariel@carmel6000.amitnet.org>
2022-02-02 18:44:15 +0200

    map/foreach

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import SortingHat from './components/SortingHat';
 import './App.css';
 import { Tardis } from './components/Tardis';
+import SWC from './components/SWC';
 
 function App() {
   return (
     <div className="App">
       {/* <SortingHat /> */}
-      <Tardis/>
+      {/* <Tardis/> */}
+      {/* <SWC /> */}
     </div>
   );
 }
